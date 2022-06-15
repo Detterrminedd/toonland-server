a1c63908a224a81b0205a6f27931fab58ad9b4d7
Shabeer Ahamed <detterminedd@gmail.com>
2022-06-15 21:26:47 +0530

    lo

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ const router = express.Router();
 const path = require("path");
 const PORT = process.env.PORT || 8800;
 dotenv.config();
+const cors = require("cors");
+const corsOptions = {
+  origin: "https://toonland.in",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 mongoose.connect(
   "mongodb+srv://developer:developer@cluster0.iaj3u.mongodb.net/?retryWrites=true&w=majority",
